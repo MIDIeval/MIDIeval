af154bbdb49c0b02c1a0de5ad1a501e0cd97fc0f
Hrishik Mishra <hrishikmishra@yahoo.com>
2013-09-22 23:45:31 +0530

    Added XY view group
    Added XY view group to allow settings for the XY controller.

---
+++ ImperiusB/MIDIeval/src/com/beta/UIControls/XYViewGroup.java
@@ -0,0 +1,20 @@
+package com.beta.UIControls;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class XYViewGroup extends ViewGroup {
+
+	public XYViewGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onLayout(boolean arg0, int arg1, int arg2, int arg3, int arg4) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
