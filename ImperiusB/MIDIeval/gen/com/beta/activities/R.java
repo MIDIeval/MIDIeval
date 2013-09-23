1be1687bef24d0c0adcecc69e2cb4cf2a1287d46
Hrishik Mishra <hrishikmishra@yahoo.com>
2013-09-23 20:15:26 +0530

    XYViewGroup

--- ImperiusB/MIDIeval/gen/com/beta/activities/R.java
+++ ImperiusB/MIDIeval/gen/com/beta/activities/R.java
@@ -128,18 +128,24 @@ containing a value of this type.
     public static final class id {
         public static final int Switch02=0x7f050007;
         public static final int Switch03=0x7f050008;
-        public static final int action_settings=0x7f050009;
+        public static final int action_settings=0x7f05000e;
         public static final int center=0x7f050000;
         public static final int leftBottom=0x7f050001;
         public static final int leftTop=0x7f050004;
         public static final int rightBottom=0x7f050002;
         public static final int rightTop=0x7f050003;
+        public static final int seek_01=0x7f05000c;
         public static final int switch1=0x7f050006;
+        public static final int switch_01=0x7f05000b;
+        public static final int text_01=0x7f05000d;
         public static final int xy_controller=0x7f050005;
+        public static final int xy_settingRow_01=0x7f05000a;
+        public static final int xy_settingtable=0x7f050009;
     }
     public static final class layout {
         public static final int activity_launcher=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int xy_viewgroup=0x7f030002;
     }
     public static final class menu {
         public static final int launcher=0x7f090000;
--- ImperiusB/MIDIeval/src/com/beta/UIControls/XYViewGroup.java
+++ ImperiusB/MIDIeval/src/com/beta/UIControls/XYViewGroup.java
@@ -2,19 +2,32 @@ package com.beta.UIControls;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-public class XYViewGroup extends ViewGroup {
+import com.beta.activities.R;
 
+public class XYViewGroup extends ViewGroup {
+	LayoutInflater layoutInflaterObj_m;
 	public XYViewGroup(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		this.layoutInflaterObj_m = LayoutInflater.from(context);
 		// TODO Auto-generated constructor stub
+		this.fn_InitViewGroup(context, this.layoutInflaterObj_m);//Function call to manually inflate the layout XML
 	}
 
 	@Override
-	protected void onLayout(boolean arg0, int arg1, int arg2, int arg3, int arg4) {
+	protected void onLayout(boolean isChanged, int left, int top, int right, int bottom) {
 		// TODO Auto-generated method stub
+		for ( short s_Count = 0; s_Count < this.getChildCount(); s_Count++) {
+			this.getChildAt(s_Count).layout(left, top, right, bottom);
+		}
 
+	}
+	
+	
+	public void fn_InitViewGroup(Context context, LayoutInflater layoutInflater){
+		layoutInflater.inflate(R.layout.xy_viewgroup, this);
 	}
 
 }
