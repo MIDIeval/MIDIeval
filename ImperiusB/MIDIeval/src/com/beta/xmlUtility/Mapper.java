db0e68336099f4a937e12ef3d95a3f92c689f770
darubramha89 <bhupen.chn@gmail.com>
2013-09-25 13:35:43 +0530

    Mapper updated for Discrete Digital
    The function getContinuosFunctionValue will be able to get values for Digital as well as Discrete, refactor it to getFunctionValue()

--- ImperiusB/MIDIeval/src/com/beta/xmlUtility/Mapper.java
+++ ImperiusB/MIDIeval/src/com/beta/xmlUtility/Mapper.java
@@ -13,23 +13,20 @@ public class Mapper
 	{
 		return context;
 	}
-	
-	public Mapper()
-	{
-		
-	}
+
 	public static void setContext(Context context) {
 		Mapper.context = context;
+		f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
 	}
-	
+
 	public static String[] getContinuos()
 	{
-		f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
+		//f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
 		ArrayList<Controllable> f_continuos_list = new ArrayList<Controllable>();
 		StringBuilder sbObj = new StringBuilder();
 		for(Controllable controllableObj  : f_controllable_list)
 		{
-			
+
 			for(int integerObj : ((Controllable) controllableObj).getF_attributes_list())
 			{
 				if(integerObj == 0)
@@ -42,6 +39,56 @@ public class Mapper
 		}
 		return sbObj.toString().split(" ");
 	}
+	
+	public static String[] getDigital()
+	{
+		//f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
+		ArrayList<Controllable> f_continuos_list = new ArrayList<Controllable>();
+		StringBuilder sbObj = new StringBuilder();
+		for(Controllable controllableObj  : f_controllable_list)
+		{
+
+			for(int integerObj : ((Controllable) controllableObj).getF_attributes_list())
+			{
+				if(integerObj == 1)
+				{
+					f_continuos_list.add((Controllable)controllableObj);
+					sbObj.append(controllableObj.getF_controllerName_s() + " ");
+				}
+			}  
+		Log.d("Object value", controllableObj.toString());
+		}
+		return sbObj.toString().split(" ");
+	}
+	
+	public static String[] getDiscrete()
+	{
+		//f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
+		ArrayList<Controllable> f_continuos_list = new ArrayList<Controllable>();
+		StringBuilder sbObj = new StringBuilder();
+		for(Controllable controllableObj  : f_controllable_list)
+		{
+
+			for(int integerObj : ((Controllable) controllableObj).getF_attributes_list())
+			{
+				if(integerObj == 2)
+				{
+					f_continuos_list.add((Controllable)controllableObj);
+					sbObj.append(controllableObj.getF_controllerName_s() + " ");
+				}
+			}  
+		Log.d("Object value", controllableObj.toString());
+		}
+		return sbObj.toString().split(" ");
+	}
+
+	/** getContinuosFunctionValue()
+	 * This function should do the role for all 
+	 * functionValues, as it iterates the whole list of controllables
+	 * 
+	 * We should refactor this and Rename it to getFunctionValue() and use for all values 
+	 */
+	
 	
 	public static int getContinuosFunctionValue(String function)
 	{
@@ -56,40 +103,67 @@ public class Mapper
 		return -25;
 	}
 	
-	public static ArrayList<Controllable> getDiscrete()
-	{
-		ArrayList<Controllable> f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
-		ArrayList<Controllable> f_continuos_list = new ArrayList<Controllable>();
-		for(Object controllableObj  : f_controllable_list)
-		{
-			for(Object integerObj : ((Controllable) controllableObj).getF_attributes_list())
-			{
-				if(integerObj.equals((Integer)2))
-				{
-					f_continuos_list.add((Controllable)controllableObj);
-				}
-			}  
-		Log.d("Object value", controllableObj.toString());
-		}
-		return f_controllable_list;
-	}
-	
-	public static ArrayList<Controllable> getDigital()
-	{
-		ArrayList<Controllable> f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
-		ArrayList<Controllable> f_continuos_list = new ArrayList<Controllable>();
-		for(Object controllableObj  : f_controllable_list)
-		{
-			for(Object integerObj : ((Controllable) controllableObj).getF_attributes_list())
-			{
-				if(integerObj.equals((Integer)1))
-				{
-					f_continuos_list.add((Controllable)controllableObj);
-				}
-			}  
-		Log.d("Object value", controllableObj.toString());
-		}
-		return f_controllable_list;
-	}
-	
 }
+//	public static int getDiscreteFunctionValue(String function)
+//	{
+//		for(Controllable controllableObj  : f_controllable_list)
+//		{
+//			  if(controllableObj.getF_controllerName_s().equals(function))
+//			  {
+//				  return controllableObj.fn_ReturnFunctionValue(function);
+//			  }
+//		Log.d("Object value", controllableObj.toString());
+//		}
+//		return -25;
+//	}
+//	
+//	public static int getDigitalFunctionValue(String function)
+//	{
+//		for(Controllable controllableObj  : f_controllable_list)
+//		{
+//			  if(controllableObj.getF_controllerName_s().equals(function))
+//			  {
+//				  return controllableObj.fn_ReturnFunctionValue(function);
+//			  }
+//		Log.d("Object value", controllableObj.toString());
+//		}
+//		return -25;
+//	}
+	
+
+
+//	public static ArrayList<Controllable> getDiscrete1()
+//	{
+//		ArrayList<Controllable> f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
+//		ArrayList<Controllable> f_continuos_list = new ArrayList<Controllable>();
+//		for(Object controllableObj  : f_controllable_list)
+//		{
+//			for(Object integerObj : ((Controllable) controllableObj).getF_attributes_list())
+//			{
+//				if(integerObj.equals((Integer)2))
+//				{
+//					f_continuos_list.add((Controllable)controllableObj);
+//				}
+//			}  
+//		Log.d("Object value", controllableObj.toString());
+//		}
+//		return f_controllable_list;
+//	}
+
+//	public static ArrayList<Controllable> getDigital()
+//	{
+//		ArrayList<Controllable> f_controllable_list = Parser.parseValues(object, "CTRL", 2, getContext());
+//		ArrayList<Controllable> f_continuos_list = new ArrayList<Controllable>();
+//		for(Object controllableObj  : f_controllable_list)
+//		{
+//			for(Object integerObj : ((Controllable) controllableObj).getF_attributes_list())
+//			{
+//				if(integerObj.equals((Integer)1))
+//				{
+//					f_continuos_list.add((Controllable)controllableObj);
+//				}
+//			}  
+//		Log.d("Object value", controllableObj.toString());
+//		}
+//		return f_controllable_list;
+//	}
